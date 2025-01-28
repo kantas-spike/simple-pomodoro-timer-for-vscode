8a63b4fed4767b31704264411c20cd38e32b9c6d
kanta <101532773+kantas-spike@users.noreply.github.com>
2025-01-28 23:26:12 +0900

    時刻表示文字列関数追加

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,4 +1,5 @@
 import * as assert from 'assert';
+import * as utils from '../../utils';
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
@@ -6,10 +7,35 @@ import * as vscode from 'vscode';
 // import * as myExtension from '../../extension';
 
 suite('Extension Test Suite', () => {
-	vscode.window.showInformationMessage('Start all tests.');
+  vscode.window.showInformationMessage('Start all tests.');
 
-	test('Sample test', () => {
-		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
-	});
+  test('Sample test', () => {
+    assert.strictEqual(-1, [1, 2, 3].indexOf(5));
+    assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+  });
+
+  test('Default Configuration', () => {
+    const config: vscode.WorkspaceConfiguration =
+      vscode.workspace.getConfiguration();
+    console.log('@@@' + config.get('simple-pomodoro-timer.defaultWorkingTime'));
+    assert.strictEqual(
+      25,
+      config.get('simple-pomodoro-timer.defaultWorkingTime'),
+    );
+    assert.strictEqual(
+      5,
+      config.get('simple-pomodoro-timer.defaultShortBreakTime'),
+    );
+    assert.strictEqual(
+      15,
+      config.get('simple-pomodoro-timer.defaultLongBreakTime'),
+    );
+  });
+
+  test('Utils test', () => {
+    assert.strictEqual('00:01', utils.millisecToHHMM(1 * 1000));
+    assert.strictEqual('00:10', utils.millisecToHHMM(10 * 1000));
+    assert.strictEqual('01:01', utils.millisecToHHMM(61 * 1000));
+    assert.strictEqual('10:10', utils.millisecToHHMM(610 * 1000));
+  });
 });
---
+++ src/utils.ts
@@ -0,0 +1,23 @@
+import * as path from 'path';
+
+const MIN = 60 * 1000;
+
+function millisecToHHMM(ms: number | null): string {
+  if (ms === null) {
+    return '--:--';
+  } else if (ms <= 0) {
+    return '00:00';
+  }
+  const min = String(Math.floor(ms / MIN));
+  const sec = String(Math.floor((ms % MIN) / 1000));
+  return `${min.padStart(2, '0')}:${sec.padStart(2, '0')}`;
+}
+
+function getAudioDirPath(
+  extensionPath: string,
+  audioDirName: string = 'audio',
+) {
+  return path.join(extensionPath, audioDirName);
+}
+
+export { millisecToHHMM, getAudioDirPath };
